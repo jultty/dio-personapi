52201c702cfbf5d1d3c4727d05b90174ccb6173c
jultty <juffff@tutanota.de>
2022-01-30 21:18:41 -0300

    refactor main application class

--- src/main/java/one/digitalinnovation/refsapi/RefsApplication.java
+++ src/main/java/one/digitalinnovation/refsapi/RefsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LinkApplication {
+public class RefsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LinkApplication.class, args);
+        SpringApplication.run(RefsApplication.class, args);
     }
 
 }
--- src/test/java/one/digitalinnovation/refsapi/RefsApplicationTests.java
+++ src/test/java/one/digitalinnovation/refsapi/RefsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LinkApplicationTests {
+class RefsApplicationTests {
 
 	@Test
 	void contextLoads() {
